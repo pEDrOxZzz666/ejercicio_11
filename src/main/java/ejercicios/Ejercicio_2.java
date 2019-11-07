20ded38987c88aeb36b0adb018d74fd634cbc009
PedrOySonia <PedrOySonia@DESKTOP-L0TQQSL>
2019-11-07 20:59:50 +0100

    Modificaciones solicitadas para que el rango sea entre 0 y 20, tenga que ser un número par

--- src/main/java/ejercicios/Ejercicio_2.java
+++ src/main/java/ejercicios/Ejercicio_2.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 //Pedro A. Ruiz
 public class Ejercicio_2 {
     
+    //CONSTANTES
+    public static final int RANGO_MIN = 0;
+    public static final int RANGO_MAX = 20;
+    
     public static void main (String[] args) {
         Scanner lector = new Scanner(System.in);
         int valorUsuari = 0;
         int llegit;
-        int numIntents = 3;
+        int numIntents = 5;
         boolean valorNOk=true;
-        for (int i=0; i<numIntents; i++){
-            System.out.print("Introdueix un valor enter entre 0 i 5: ");
+        
+        
+        for (int i=0; i<numIntents;){
+            System.out.print("Introdueix un valor enter entre " + RANGO_MIN + " i " + RANGO_MAX + ": ");
             llegit = lector.nextInt();
             lector.nextLine();
-            if((llegit >= 0)&&(llegit <= 5)){
+            if((llegit >= RANGO_MIN)&&(llegit <= RANGO_MAX)&&(llegit%2==0)){
                valorUsuari += llegit; 
                    //es el mateix que  valorUsuari= valorUsuari + llegit
+               i++;
             }
         }
         System.out.println("Dada correcta. Has escrit " + valorUsuari);
